81e7acb44969e9c88b31bdebf2b2388d4b0587a8
Derek Oh <derek.oh93@gmail.com>
2017-05-06 10:56:05 -0700

    fixed some issues

--- test/spec/controllers/about.js
+++ test/spec/controllers/about.js
@@ -3,7 +3,7 @@
 describe('Controller: AboutCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('angularAppApp'));
+  beforeEach(module('TrafficApp'));
 
   var AboutCtrl,
     scope;
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('angularAppApp'));
+  beforeEach(module('TrafficApp'));
 
   var MainCtrl,
     scope;
