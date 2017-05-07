a60156c93d381d0e6b36fe2e5f3f2b95527d0127
Derek Oh <derek.oh93@gmail.com>
2017-05-07 11:22:41 -0700

    new app

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,4 +10,10 @@
  angular.module('GetYourWeather')
    .controller('MainCtrl', function ($scope, current) {
      $scope.current = current.query();
+
+     $scope.refreshCurrent = function(){
+         $scope.current = current.query({
+             location: $scope.location
+         });
+     };
    });
