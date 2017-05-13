a73c2a934af6a8155bc535fdeab6163ac0637559
Derek Oh <derek.oh93@gmail.com>
2017-05-12 20:41:34 -0700

    city search

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,13 +7,14 @@
  * # MainCtrl
  * Controller of the GetYourWeather
  */
- angular.module('GetYourWeather')
-   .controller('MainCtrl', function ($scope, current) {
-     $scope.current = current.query();
+ angular.module('GetYourWeatherApp')
+   .controller('MainCtrl', function ($scope, citysearch) {
+     $scope.citiesFound = citysearch.find();
 
-     $scope.refreshCurrent = function(){
-         $scope.current = current.query({
-             location: $scope.location
+     $scope.findCities = function(){
+         $scope.citiesFound = citysearch.find({
+             query: $scope.location
          });
+         $scope.searchQuery = $scope.location;
      };
    });
---
+++ app/scripts/services/citysearch.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name GetYourWeatherApp.citysearch
+ * @description
+ * # citysearch
+ * Factory in the GetYourWeatherApp.
+ */
+
+
+ angular.module('GetYourWeatherApp')
+   .factory('citysearch', function ($resource) {
+     // Service logic
+     // ...
+
+     // Public API here
+     return $resource('http://api.openweathermap.org/data/2.5/find?q=:query&type=like&mode=json&APPID=0abed609952210f52ec8119239c1db6b', {}, {
+       find: {
+         method: 'GET',
+         params: {
+           query: 'seattle'
+         },
+         isArray: false
+       }
+     });
+   });
