cde844705b4184e5d24a09bb4b8bad407ab6a917
Derek Oh <derek.oh93@gmail.com>
2017-05-13 22:59:36 -0700

    fixes

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -8,10 +8,33 @@
  * Controller of the GetYourWeatherApp
  */
  angular.module('GetYourWeather')
-   .controller('CurrentCtrl', function ($scope, $routeParams, current) {
+   .controller('CurrentCtrl', function ($scope, $routeParams, current, $localStorage) {
      $scope.cityID = $routeParams.cityID;
 
      $scope.currentWeather = current.query({
          cityID: $routeParams.cityID
      });
+     $scope.saveCity = function(city){
+         var cityData = {
+             'name': city.name,
+             'id': city.id
+         };
+         if (!$localStorage.savedCities){
+             $localStorage.savedCities = [cityData];
+         } else {
+             // Check to make sure we haven't already saved the city.
+             var save = true;
+             for (var i=0; i < $localStorage.savedCities.length; i++){
+                 if ($localStorage.savedCities[i].id == cityData.id) {
+                     // this is a duplicate, so don't save
+                     save = false;
+                 }
+             }
+             if (save==true){
+                 $localStorage.savedCities.push(cityData);
+             } else {
+                 console.log('city already saved');
+             }
+         }
+     };
    });
